6c02f9f40f49597231b285c8259bc56062807f5a
zackzellto <zacharyzellto@gmail.com>
2021-11-05 18:42:07 -0600

    Third Commit - Added Delete button on row creation

--- app.js
+++ app.js
@@ -5,13 +5,6 @@ const headerLoc = document.getElementById('purchase-location');
 const headerDesc = document.getElementById('purchase-desc');
 const headerAmount = document.getElementById('purchase-amount');
 
-//Table Row Variables
-const newPaymentType = document.getElementById('new-type').textContent;
-const newPurchaseDate = document.getElementById('new-date').textContent;
-const newPurchaseLoc = document.getElementById('new-location').textContent;
-const newPurchaseDesc = document.getElementById('new-desc').textContent;
-const newPurchaseAmount = document.getElementById('new-amount').textContent;
-
 //Input Variables
 const inputType = document.getElementById('input-type');
 const inputDate = document.getElementById('input-date');
@@ -19,23 +12,29 @@ const inputLoc = document.getElementById('input-location');
 const inputDesc = document.getElementById('input-desc');
 const inputAmount = document.getElementById('input-amount');
 
-//Expense Button
-const expenseBtn = document.getElementById('add-btn');
+//Delete Expense Function
 
-function deleteExpense(){
-
+function deleteExpense(e){
+    e.target.parentElement.childElement.remove();
 }
 
 
+//Add Expense Function
 function addExpense(){
     let i = 1 ;
-    let newRow = document.createElement('TD');
     let row = document.getElementById('expense-table').insertRow(i);
     let typeCell = row.insertCell(0);
     let dateCell = row.insertCell(1);
     let locationCell = row.insertCell(2);
     let descriptionCell = row.insertCell(3);
     let amountCell = row.insertCell(4);
+    let removeExpCell = row.insertCell(5);
+    let newDeleteBtn = document.createElement('button');
+    newDeleteBtn.setAttribute('id','delete-button')
+    newDeleteBtn.textContent = "Delete Expense"
+    newDeleteBtn.addEventListener('click', () => deleteExpense());
+
+    removeExpCell.appendChild(newDeleteBtn);
 
     typeCell.textContent = inputType.value;
     dateCell.textContent = inputDate.value;
